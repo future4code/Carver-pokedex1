7def4b22d634efc8244ee85861feeffc631ca657
pedrohenriqueduarte <ph09duarte@gmail.com>
2022-01-04 17:18:35 -0300

    ajustes

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -3,6 +3,8 @@ import Router from './router/Router';
 import { createGlobalStyle } from 'styled-components';
 import { ThemeProvider } from '@material-ui/core';
 import theme from './constants/theme';
+import useRequestData from './hooks/useRequestData';
+import { PokemonContext } from './dataContext/PokemonContext';
 
 const GlobalStyle = createGlobalStyle`
 body{
@@ -14,10 +16,17 @@ body{
 
 
 const App = () => {
+
+  const pokeList = useRequestData([], 'https://pokeapi.co/api/v2/pokemon/1')
+
+  console.log(pokeList)
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
+      <PokemonContext.Provider value={pokeList}>
       <Router/>
+      </PokemonContext.Provider>
     </ThemeProvider>
   );
 }
